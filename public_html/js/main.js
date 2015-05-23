6e0ad529065cff5ea7d4a733763b06e2247192af
idaho <idaho@streetfighter2>
2015-05-23 18:59:48 +0200

    Arreglamos la licencia.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,21 +1,8 @@
 /* 
- * Copyright (C) 2015 idaho
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Copyright (C) César Rodríguez Moreno, AKA Idaho
+ * Thanks go to Rx, Palo, MadJ0ker, DarkHeavy, Ikky, 
+ * The Aberred Crew and all you know who.
  */
-
 
 var scene, camera, renderer;
 var geometry, material, mesh, light;
@@ -37,7 +24,7 @@ function init() {
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    scene.add(new THREE.AmbientLight(0x000000));
+    scene.add(new THREE.AmbientLight(0x111111));
 
     light = new THREE.PointLight(0xff0040, 2, 1000, 2);
 
